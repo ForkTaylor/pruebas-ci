f0fcdde6c0228f1eaa4c4425a6aef4b038ee006e
Raul Garcia <skittleguy00@gmail.com>
2021-09-01 19:59:28 -0500

    Nueva prueba de resta agregada

--- __tests__/challenge1.test.js
+++ __tests__/challenge1.test.js
@@ -8,3 +8,11 @@ test("Deberia regresar 8 al pasar como atgumento 6 y 2", () => {
 
     expect(result).toBe(8);
 });
+
+test("Debería regresar 4 al ingresar 8 y 4", () => {
+    let a = 8;
+    let b = 4;
+
+    let result = (resta(a,b);
+    expect(result).toBe(4);
+})
